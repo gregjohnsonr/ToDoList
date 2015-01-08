b982162b9d138a816f9ad673b1f93fd7c140672e
Greg Johnson <greg@GregsMacPro.local>
2015-01-07 19:02:50 -0800

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Greg Johnson on 1/7/15.
+//  Copyright (c) 2015 Greg Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
